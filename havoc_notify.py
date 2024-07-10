900ee2ea444259cb04f3aeeb4161a0f85025f805
rayaars <raymond.aarseth@soprasteria.com>
2024-07-10 09:20:11 +0200

    Removed debug prints and fixed Teams formating

--- havoc_notify.py
+++ havoc_notify.py
@@ -2,6 +2,8 @@ import os
 import requests
 import havocui
 from havoc import Demon, Event
+import concurrent.futures
+
 
 # Global variables for Pushover keys and selected items
 pushover_config = {
@@ -34,31 +36,29 @@ notified_demons_file = os.path.join(config_dir, "notified_demons.txt")
 options_file = os.path.join(config_dir, "options_config.txt")
 
 
+
+
 # Function to load configuration from file
 def load_config(service):
     config_path = os.path.join(config_dir, f"{service}_config.txt")
-    print(f"Loading config for {service} from {config_path}")
     if os.path.exists(config_path):
         with open(config_path, "r") as file:
             lines = file.readlines()
             for line in lines:
                 parts = line.strip().split(":")
                 if len(parts) < 2:
-                    print(f"Skipping malformed line: {line}")
                     continue
                 key, value = parts[0], ":".join(parts[1:])  # Rejoin in case value contains colons
                 if service == 'pushover':
                     pushover_config[key] = value
                 elif service == 'teams':
                     teams_config[key] = value
-    print(f"Loaded config for {service}: {pushover_config if service == 'pushover' else teams_config}")
 
 
 
 # Function to save configuration to file
 def save_config(service):
     config_path = os.path.join(config_dir, f"{service}_config.txt")
-    print(f"Saving config for {service} to {config_path}")
     with open(config_path, "w") as file:
         if service == 'pushover':
             for key, value in pushover_config.items():
@@ -66,22 +66,18 @@ def save_config(service):
         elif service == 'teams':
             for key, value in teams_config.items():
                 file.write(f"{key}:{value}\n")
-    print(f"Saved config for {service}")
 
 # Function to load options from file
 def load_options():
-    print(f"Loading options from {options_file}")
     if os.path.exists(options_file):
         with open(options_file, "r") as file:
             lines = file.readlines()
             for line in lines:
                 key, value = line.strip().split(":")
                 globals()[key] = value == 'True'
-    print(f"Loaded options: {globals()}")
 
 # Function to save options to file
 def save_options():
-    print(f"Saving options to {options_file}")
     with open(options_file, "w") as file:
         options = [
             'save_keys', 'send_listener', 'send_external_ip', 'send_internal_ip',
@@ -90,7 +86,6 @@ def save_options():
         ]
         for option in options:
             file.write(f"{option}:{globals()[option]}\n")
-    print(f"Saved options")
 
 
 
@@ -111,10 +106,15 @@ def save_notified_demon(demonID):
     with open(notified_demons_file, "a") as file:
         file.write(f"{demonID}\n")
 
+
+def threaded_send_notification(facts, pushover_message):
+    threading.Thread(target=send_pushover_notification, args=(pushover_message,)).start()
+    threading.Thread(target=send_teams_notification, args=(facts,)).start()
+
+
 # Function to send Pushover notification
 def send_pushover_notification(message):
     if not pushover_config['user_key'] or not pushover_config['api_token']:
-        print("Pushover keys not set, skipping notification")
         return
     payload = {
         'token': pushover_config['api_token'],
@@ -122,12 +122,12 @@ def send_pushover_notification(message):
         'message': message
     }
     response = requests.post('https://api.pushover.net/1/messages.json', data=payload)
-    print(f"Pushover response: {response.status_code}, {response.text}")
+
+
 
 # Function to send Teams notification
-def send_teams_notification(message):
+def send_teams_notification(facts):
     if not teams_config['webhook_url']:
-        print("Teams webhook URL not set, skipping notification")
         return
 
     payload = {
@@ -139,56 +139,85 @@ def send_teams_notification(message):
                     "type": "AdaptiveCard",
                     "body": [
                         {
-                            "type": "TextBlock",
-                            "text": message,
-                            "wrap": True
+                            "type": "Container",
+                            "items": [
+                                {
+                                    "type": "TextBlock",
+                                    "text": "New Demon Connection",
+                                    "weight": "Bolder",
+                                    "size": "Medium"
+                                },
+                                {
+                                    "type": "TextBlock",
+                                    "text": "New demon connected with the following details:",
+                                    "wrap": True
+                                },
+                                {
+                                    "type": "FactSet",
+                                    "facts": facts
+                                }
+                            ]
                         }
                     ],
-                    "version": "1.0"
+                    "version": "1.2"
                 }
             }
         ]
     }
 
     response = requests.post(teams_config['webhook_url'], json=payload)
-    print(f"Teams response: {response.status_code}, {response.text}")
 
 # Callback function for new daemon connections
 def alert_new_demon(demonID):
-    if demonID in notified_demons:
-        print(f"Daemon {demonID} already notified.")
-        return
     try:
         demon = Demon(demonID)
-        message = (
-           "New demon connected\n"
-           f"Listener: {demon.Listener if demon.Listener else 'None'}\n"
-           f"External IP: {demon.ExternalIP if demon.ExternalIP else 'None'}\n"
-           f"Internal IP: {demon.InternalIP if demon.InternalIP else 'None'}\n"
-           f"Username: {demon.User if demon.User else 'None'}\n"
-           f"Hostname: {demon.Computer if demon.Computer else 'None'}\n"
-           f"Domain: {demon.Domain if demon.Domain else 'None'}\n"
-           f"OS: {demon.OS if demon.OS else 'None'}\n"
-           f"OS Build: {demon.OSBuild if demon.OSBuild else 'None'}\n"
-           f"OS Architecture: {demon.OSArch if demon.OSArch else 'None'}\n"
-           f"Process Name: {demon.ProcessName if demon.ProcessName else 'None'}\n"
-           f"Process ID: {demon.ProcessID if demon.ProcessID else 'None'}\n"
-           f"Process Architecture: {demon.ProcessArch if demon.ProcessArch else 'None'}"
-        )
+        facts = []
+        pushover_message = "New demon connected:"
 
-        print(f"Sending notification for new demon: {message}")
-        send_pushover_notification(message)
-        send_teams_notification(message)
+        if send_listener and demon.Listener:
+            facts.append({"title": "Listener:", "value": demon.Listener})
+            pushover_message += f"\nListener: {demon.Listener}"
+        if send_external_ip and demon.ExternalIP:
+            facts.append({"title": "External IP:", "value": demon.ExternalIP})
+            pushover_message += f"\nExternal IP: {demon.ExternalIP}"
+        if send_internal_ip and demon.InternalIP:
+            facts.append({"title": "Internal IP:", "value": demon.InternalIP})
+            pushover_message += f"\nInternal IP: {demon.InternalIP}"
+        if send_username and demon.User:
+            facts.append({"title": "Username:", "value": demon.User})
+            pushover_message += f"\nUsername: {demon.User}"
+        if send_hostname and demon.Computer:
+            facts.append({"title": "Hostname:", "value": demon.Computer})
+            pushover_message += f"\nHostname: {demon.Computer}"
+        if send_domain and demon.Domain:
+            facts.append({"title": "Domain:", "value": demon.Domain})
+            pushover_message += f"\nDomain: {demon.Domain}"
+        if send_os and demon.OS:
+            facts.append({"title": "OS:", "value": demon.OS})
+            pushover_message += f"\nOS: {demon.OS}"
+        if send_os_build and demon.OSBuild:
+            facts.append({"title": "OS Build:", "value": demon.OSBuild})
+            pushover_message += f"\nOS Build: {demon.OSBuild}"
+        if send_os_arch and demon.OSArch:
+            facts.append({"title": "OS Architecture:", "value": demon.OSArch})
+            pushover_message += f"\nOS Architecture: {demon.OSArch}"
+        if send_process_name and demon.ProcessName:
+            facts.append({"title": "Process Name:", "value": demon.ProcessName})
+            pushover_message += f"\nProcess Name: {demon.ProcessName}"
+        if send_process_id and demon.ProcessID:
+            facts.append({"title": "Process ID:", "value": demon.ProcessID})
+            pushover_message += f"\nProcess ID: {demon.ProcessID}"
+        if send_process_arch and demon.ProcessArch:
+            facts.append({"title": "Process Architecture:", "value": demon.ProcessArch})
+            pushover_message += f"\nProcess Architecture: {demon.ProcessArch}"
 
-        notified_demons.add(demonID)
-        save_notified_demon(demonID)
+        threaded_send_notification(facts, pushover_message)
     except Exception as e:
         print(f"Exception occurred in alert_new_demon: {e}")
 
 
 # Pushover GUI
 def open_pushover_gui():
-    print("Opening Pushover GUI")
     try:
         load_config('pushover')
         pushover_gui.clear()
@@ -203,19 +232,15 @@ def open_pushover_gui():
         print(f"Exception occurred while setting up Pushover GUI: {e}")
 
 def set_pushover_user_key(text):
-    print(f"Setting Pushover user key: {text}")
     pushover_config['user_key'] = text
 
 def set_pushover_api_token(text):
-    print(f"Setting Pushover API token: {text}")
     pushover_config['api_token'] = text
 
 def save_pushover_config():
-    print("Saving Pushover config")
     save_config('pushover')
 # Teams GUI
 def open_teams_gui():
-    print("Opening Teams GUI")
     try:
         load_config('teams')
         teams_gui.clear()
@@ -224,22 +249,18 @@ def open_teams_gui():
         teams_gui.addLineedit(teams_config['webhook_url'], set_teams_webhook_url)
         teams_gui.addButton("Save Configuration", save_teams_config)
         teams_gui.setSmallTab()
-        print("Teams GUI setup complete")
     except Exception as e:
         print(f"Exception occurred while setting up Teams GUI: {e}")
 
 def set_teams_webhook_url(text):
-    print(f"Setting Teams webhook URL: {text}")
     teams_config['webhook_url'] = text
 
 def save_teams_config():
-    print("Saving Teams config")
     save_config('teams')
 
 
 # Options GUI
 def open_options_gui():
-    print("Opening Options GUI")
     try:
         options_gui.clear()
         options_gui.addLabel("<h3 style='color:#bd93f9'>Options</h3>")
@@ -263,79 +284,66 @@ def toggle_save_keys():
     global save_keys
     save_keys = not save_keys
     save_options()
-    print(f"Toggled save_keys to {save_keys}")
 
 def toggle_send_listener():
     global send_listener
     send_listener = not send_listener
     save_options()
-    print(f"Toggled send_listener to {send_listener}")
 
 def toggle_send_external_ip():
     global send_external_ip
     send_external_ip = not send_external_ip
     save_options()
-    print(f"Toggled send_external_ip to {send_external_ip}")
 
 def toggle_send_internal_ip():
     global send_internal_ip
     send_internal_ip = not send_internal_ip
     save_options()
-    print(f"Toggled send_internal_ip to {send_internal_ip}")
 
 def toggle_send_username():
     global send_username
     send_username = not send_username
     save_options()
-    print(f"Toggled send_username to {send_username}")
 
 def toggle_send_hostname():
     global send_hostname
     send_hostname = not send_hostname
     save_options()
-    print(f"Toggled send_hostname to {send_hostname}")
 
 def toggle_send_domain():
     global send_domain
     send_domain = not send_domain
     save_options()
-    print(f"Toggled send_domain to {send_domain}")
 
 def toggle_send_os():
     global send_os
     send_os = not send_os
     save_options()
-    print(f"Toggled send_os to {send_os}")
 
 def toggle_send_os_build():
     global send_os_build
     send_os_build = not send_os_build
     save_options()
-    print(f"Toggled send_os_build to {send_os_build}")
 
 def toggle_send_os_arch():
     global send_os_arch
     send_os_arch = not send_os_arch
     save_options()
-    print(f"Toggled send_os_arch to {send_os_arch}")
 
 def toggle_send_process_name():
     global send_process_name
     send_process_name = not send_process_name
     save_options()
-    print(f"Toggled send_process_name to {send_process_name}")
 
 def toggle_send_process_id():
     global send_process_id
     send_process_id = not send_process_id
     save_options()
-    print(f"Toggled send_process_id to {send_process_id}")
 
 def toggle_send_process_arch():
     global send_process_arch
     send_process_arch = not send_process_arch
     save_options()
-    print(f"Toggled send_process_arch to {send_process_arch}")
 
 
 # Load configuration for Pushover and Teams
